eb7ea55aa882d0f0bbceaf8ab3e3c544f9f3340e
Atif Mahmud <atifmahmud95@gmail.com>
2020-04-04 17:31:17 -0700

    Changes before bugfix

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,34 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class CharacterController : MonoBehaviour
 {
 
-    private Rigidbody rb;
+    private Rigidbody2D rb;
     private Camera camera;
     private Animator animator;
+    private bool grounded = true;
+    private AudioSource[] audios;
+    private int score = 0;
+
+    GameObject scoreText;
+
+    // for debugging
+    public float jumpForce;
+    public float run;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("@@Start");
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
         camera = Camera.main;
         animator = GetComponent<Animator>();
+        audios = GetComponents<AudioSource>();
+        scoreText = GameObject.Find("ScoreNum");
+        SetScore();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetFloat("Speed", 0.0f);
+        animator.SetBool("Jump", false);
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             MoveRight();
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // MoveLeft ();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
@@ -36,14 +56,57 @@ public class CharacterController : MonoBehaviour
 
     void MoveRight()
     {
-        this.transform.Translate(0.25f, 0, 0);
-        camera.transform.Translate(0.25f, 0, 0);
+        this.transform.Translate(run, 0, 0);
+        camera.transform.Translate(run, 0, 0);
+        animator.SetFloat("Speed", 2.0f);
+    }
+
+
+    // Under Construction
+    void MoveLeft()
+    {
+        gameObject.transform.transform.Rotate(0, 180, 0, Space.Self);
+        this.transform.Translate(-run, 0, 0);
+        camera.transform.Translate(-0.75f * run, 0, 0);
         animator.SetFloat("Speed", 2.0f);
     }
 
     void Jump()
     {
-        Debug.Log("@@ Jump");
-        this.transform.Translate(0, 0.25f, 0);
+        if (grounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce));
+            animator.SetBool("Jump", true);
+            audios[0].Play();
+            grounded = false;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")){
+            grounded = true;
+            // Debug.Log("@@ Grounded");
+        }
+
+        if (collision.gameObject.CompareTag("Pickup"))
+        {
+            Destroy(collision.gameObject);
+            IncrementScore();
+            audios[1].Play();           
+        }
+    }
+
+    void IncrementScore()
+    {
+        score++;
+        SetScore();
+    }
+
+    void SetScore()
+    {
+        scoreText.GetComponent<TMPro.TextMeshProUGUI>().text = score.ToString();
     }
 }
+
